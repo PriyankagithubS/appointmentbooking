a99cc184af55d3e6542bf2e9f3d066284a2ae64f
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-11-04 11:35:04 +0530

    error rectified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Doctors from './pages/Doctors';
